792819fdb27ab0ad103f9462c3fe2b291097fe24
ThrackersOdd <totalynotpeeloff@gmail.com>
2025-11-17 10:17:24 -0800

    Update Driver.java

--- Driver.java
+++ Driver.java
@@ -1,6 +1,96 @@
 public class Driver extends Person {
 
-    private String vehicle;
+     private String vehicle;
+		private double odometer = 0;
+		private double raiting = 0;
+		private String carModel = "1978 Boogati";
+		private String currentOrder;
+		private String deliveryTime;
+
+
+
+
+//Temp from order since I need it for code
+		private double distance = 5.5;
+
+
+	public boolean isDriver(){
+				boolean verify = false;
+
+		if(userID.charAt(0) == 'D'){
+			verify = true;
+		}
+		return verify;
+	}
+
+
+
+	public void setOdometer(){
+		odometer = odometer + distance;
+	}
+
+
+
+
+	public double getOdometer(){
+
+		return odometer;
+	}
+
+
+
+
+	public double getRaiting(){
+
+		return -1;
+	}
+
+
+
+
+	public void setCarModel(){
+		
+	}
+
+
+
+
+	public String getCarModel(){
+
+		return "";
+	}
+
+
+
+
+	public void setCurrentOrder(){
+		
+	}
+
+
+
+
+
+	public String getCurrentOrder(){
+		return "";
+	}
+
+
+
+
+
+	public String getDeliveryAddress(){
+		return "";
+	}
+
+
+
+
+	public String viewOrder(){
+		return "";
+	}
+
+
     
 
   
